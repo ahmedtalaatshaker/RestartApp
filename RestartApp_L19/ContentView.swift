b212bac8a1b91b0c20395e88e32a811a76b7f787
Ahmed Talaat <ahmedtal@banquemisr.com>
2022-11-27 12:47:22 +0200

    Initial Commit

---
+++ RestartApp_L19/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RestartApp_L19
+//
+//  Created by Ahmed Talaat on 27/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RestartApp_L19/RestartApp_L19App.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp_L19App.swift
+//  RestartApp_L19
+//
+//  Created by Ahmed Talaat on 27/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp_L19App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
